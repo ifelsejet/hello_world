dd0440a9bbdaa786cefbb9907719dc3c929d1fd4
Jatnael Montes <ifelsejet@gmail.com>
2019-07-22 12:06:11 -0700

    Now using Jinja to serve the HTML

--- main.py
+++ main.py
@@ -2,11 +2,25 @@
 # the import section
 import webapp2
 import logging
+#Step 1: Import Jinja and os
+import jinja2
+import os
+
+#Step 2: Set up Jinja environment
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
 
 # the handler section
 class MainPage(webapp2.RequestHandler):
     def get(self): #for a get request
         logging.info("In hello handler")
+        #Step 3: Use the Jinja environment to get our HTML
+        template = jinja_env.get_template("hello.html")
+        self.response.write(template.render())
+
+
+        '''
         self.response.write("<h2> Hello CSSI <h2>")
         self.response.write("<i> I hope you're having a fun week <i>")
         self.response.write("<ul>")
@@ -14,7 +28,8 @@ class MainPage(webapp2.RequestHandler):
         self.response.write("<li> Pterodactyl")
         self.response.write("<li> Evolution")
         self.response.write("<ul>")
-
+        ^^^^ GROSS ^^^^
+        '''
 
         #self.response.write('<b>Hello, <i>CSSI!<i><b>') #the response
 
